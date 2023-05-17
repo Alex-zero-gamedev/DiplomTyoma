bf6a52131a470666706afa0e4d3c00acb24a60c7
Alex-zero-gamedev <77914959+Alex-zero-gamedev@users.noreply.github.com>
2023-05-17 23:52:38 +0300

    ы

--- Repository/Models/Users/Admin.cs
+++ Repository/Models/Users/Admin.cs
@@ -8,6 +8,7 @@ using System.Threading.Tasks;
 
 namespace Repository.Models.Users
 {
+    [Table("Администраторы")]
     public class Admin
     {
         [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
