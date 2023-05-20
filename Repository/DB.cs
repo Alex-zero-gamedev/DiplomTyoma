ea39e3b42b1958e975b32f889c282679d95ded18
Alex-zero-gamedev <77914959+Alex-zero-gamedev@users.noreply.github.com>
2023-05-20 13:38:33 +0300

    s

--- Repository/DB.cs
+++ Repository/DB.cs
@@ -39,6 +39,6 @@ namespace Repository
         public DbSet<Journal> Journals { get; set; }
         public DbSet<Special> Specials { get; set; }
         public DbSet<TutorialProgram> TutorialPrograms { get; set; }
-        public DbSet<CalendarTutorial> TutorialPrograms { get; set; }
+        public DbSet<CalendarTutorial> CalendarTutorials { get; set; }
     }
 }
